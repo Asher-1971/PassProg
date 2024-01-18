1e2cab4a28ab047846fd2ec9e93b2c845c0fa723
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-18 19:01:50 +0500

    lol

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -1,13 +1,34 @@
 Imports System
+Imports System.IO
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("")
-        Console.WriteLine("did this on vs code")
-        Console.WriteLine("did this on visual stuidio desktop ")
+    Dim Password As String
 
-
+    Sub Choices()
+        Console.WriteLine("1. Enter a new password.")
+        Console.WriteLine("2. Check your password.")
+        Console.WriteLine("3. Change your password.")
+        Console.WriteLine("4. Quit.")
+    End Sub
+    Sub PasswordInput()
+        FileOpen(1, "Password.txt", OpenMode.Output)
+        Console.Write("Enter Password:")
+        Password = Console.ReadLine()
+    End Sub
+    Sub Main(`)
+        Dim ChoiceOption As Integer
+        Do
+            Call Choices()
+            ChoiceOption = 0
+            While ChoiceOption > 4 Or ChoiceOption < 1
+                Console.Write("Enter your choice: ")
+                ChoiceOption = Console.ReadLine
+            End While
+            Select Case ChoiceOption
+                Case 1
+                Case 2
+                Case 3
+            End Select
+        Loop Until ChoiceOption = 4
     End Sub
 End Module
