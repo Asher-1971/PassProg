4c0a6c3c663aef403a310c2298a4398584e30852
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-19 00:14:15 +0500

    me

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -5,11 +5,19 @@ Module Program
     Dim Password As String
 
     Sub Choices()
-        Console.WriteLine("1. Enter a new password.")
-        Console.WriteLine("2. Check your password.")
-        Console.WriteLine("3. Change your password.")
-        Console.WriteLine("4. Quit.")
+        Console.WriteLine("========PASSWORD MANAGER========")
+        Console.WriteLine("===========MAIN MENU============")
+        Console.WriteLine("===(1) Enter a new password.====")
+        Console.WriteLine("===(2) Check the password.======")
+        Console.WriteLine("===(3) Change the password.====")
+        Console.WriteLine("===(4) Exit the program.=======")
+        Console.WriteLine("================================")
+        Console.write("Enter your choice (1-4):")
     End Sub
+   Sub Main()
+       
+    End Sub 
+    
     Sub PasswordInput()
         Dim CorrectLength, NoSpaces, UpperCasePresent, DigitPresent As Boolean
         FileOpen(1, "Password.txt", OpenMode.Output)
@@ -32,21 +40,5 @@ Module Program
         PrintLine(1, Password)
         FileClose(1)
     End Sub
-    Sub Main()
-        Dim ChoiceOption As Integer
-        Do
-            Call Choices()
-            ChoiceOption = 0
-            While ChoiceOption > 4 Or ChoiceOption < 1
-                Console.Write("Enter your choice: ")
-                ChoiceOption = Console.ReadLine
-            End While
-            Select Case ChoiceOption
-                Case 1
-                    Call PasswordInput()
-                Case 2
-                Case 3
-            End Select
-        Loop Until ChoiceOption = 4
-    End Sub
+    
 End Module
