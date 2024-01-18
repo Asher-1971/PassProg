fffe04c1c8f900197fa617648fff0149751332b8
Asher-1971 <aliasher.0834@bvsparsischool.edu.pk>
2024-01-19 00:14:45 +0500

    le

--- LossyLab/Program.vb
+++ LossyLab/Program.vb
@@ -15,7 +15,31 @@ Module Program
         Console.write("Enter your choice (1-4):")
     End Sub
    Sub Main()
-       
+        Dim ChoiceOption As Integer
+        ChoiceOption = console.ReadLine()
+
+        while ChoiceOption > 4 or ChoiceOption < 1
+            Console.WriteLine("Invalid choice. Please try again.")
+            Console.WriteLine()
+            Choices()
+            ChoiceOption = console.ReadLine()
+        End While
+
+        Select Case ChoiceOption
+            Case 1
+                PasswordInput()
+            Case 2
+                PasswordCheck()
+            Case 3
+                PasswordChange()
+            Case 4
+                Console.WriteLine("Thank you for using the Password Manager.")
+                Console.WriteLine("Goodbye.")
+                Console.WriteLine()
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey()
+                Environment.Exit(0)
+        End Select
     End Sub 
     
     Sub PasswordInput()
